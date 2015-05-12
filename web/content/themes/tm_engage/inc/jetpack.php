168ab1d496426d1c5b583a35ca6365a27270f6af
Michael Bragg <michael@michaelbragg.net>
2015-05-12 11:41:10 +0100

    Add WordPress

---
+++ web/content/themes/tm_engage/inc/jetpack.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Jetpack Compatibility File
+ * See: https://jetpack.me/
+ *
+ * @package tm_engage
+ */
+
+/**
+ * Add theme support for Infinite Scroll.
+ * See: https://jetpack.me/support/infinite-scroll/
+ */
+function tm_engage_jetpack_setup() {
+	add_theme_support( 'infinite-scroll', array(
+		'container' => 'main',
+		'render'    => 'tm_engage_infinite_scroll_render',
+		'footer'    => 'page',
+	) );
+} // end function tm_engage_jetpack_setup
+add_action( 'after_setup_theme', 'tm_engage_jetpack_setup' );
+
+function tm_engage_infinite_scroll_render() {
+	while ( have_posts() ) {
+		the_post();
+		get_template_part( 'template-parts/content', get_post_format() );
+	}
+} // end function tm_engage_infinite_scroll_render
