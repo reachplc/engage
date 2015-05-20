f67a902dfba706e45107b71aa17604a54e7f0ac0
Michael Bragg <michael@michaelbragg.net>
2015-05-20 15:31:45 +0100

    Add news pages

--- web/content/themes/tm_engage/template-parts/content.php
+++ web/content/themes/tm_engage/template-parts/content.php
@@ -4,35 +4,25 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'archive-news' ); ?>>
 	<header class="entry-header">
+
+  <?php if( has_post_thumbnail( $post->ID ) ): ?>
+  	<div>
+	  <?php echo get_the_post_thumbnail( $post->ID ); ?>
+	  </div>
+	<?php else: ?>
+		<img src="<?php echo bloginfo( 'template_directory' ); ?>/gui/feature-placeholder.png" alt="">
+  <?php endif; ?>
+
 		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
 
-		<?php if ( 'post' == get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php tm_engage_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
-		<?php
-			/* translators: %s: Name of current post */
-			the_content( sprintf(
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'tm_engage' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
-		?>
 
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'tm_engage' ),
-				'after'  => '</div>',
-			) );
-		?>
+	<?php the_excerpt(); ?>
+
 	</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-		<?php tm_engage_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
