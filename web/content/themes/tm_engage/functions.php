8fa666117a5eb914ac7751ceb3c5455202a9fe6d
Jon Masters <Jonathan_masters77@yahoo.co.uk>
2015-07-29 14:31:08 +0100

    Target. Connect. Engage. fading text fix

--- web/content/themes/tm_engage/functions.php
+++ web/content/themes/tm_engage/functions.php
@@ -138,11 +138,11 @@ function tm_engage_scripts() {
 	wp_enqueue_script( 'tm_engage-easing', get_template_directory_uri() . '/js/jquery.easing.1.3.js', array( 'jquery' ), '1.3', true );
 	wp_enqueue_script( 'tm_engage-camera', get_template_directory_uri() . '/js/camera.js', array( 'jquery' ), '1.3.3', true );
 	wp_enqueue_script( 'tm_engage-waypoints', get_template_directory_uri() . '/js/waypoints.min.js', array( 'jquery' ), '2.0.3', true );
-	wp_enqueue_script( 'tm_engage-classie', get_template_directory_uri() . '/js/classie.js', '20150519', true );
-	wp_enqueue_script( 'tm_engage-stepsform', get_template_directory_uri() . '/js/stepsForm.js', '1.0.0', true );
+	wp_enqueue_script( 'tm_engage-classie', get_template_directory_uri() . '/js/classie.js', array( 'jquery' ), '20150519', true );
+	wp_enqueue_script( 'tm_engage-stepsform', get_template_directory_uri() . '/js/stepsForm.js', array( 'jquery' ), '1.0.0', true );
 	wp_enqueue_script( 'tm_engage-bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array( 'jquery' ), '3.1.1', true );
-	wp_enqueue_script( 'tm_engage-flexslider', get_template_directory_uri() . '/js/flexslider.min.js', '20150519', true );
-	wp_enqueue_script( 'tm_engage-parallax', get_template_directory_uri() . '/js/parallax.min.js', '20150519', true );
+	wp_enqueue_script( 'tm_engage-flexslider', get_template_directory_uri() . '/js/flexslider.min.js', array( 'jquery' ), '20150519', true );
+	wp_enqueue_script( 'tm_engage-parallax', get_template_directory_uri() . '/js/parallax.min.js', array( 'jquery' ), '20150519', true );
 	wp_enqueue_script( 'tm_engage-scripts', get_template_directory_uri() . '/js/scripts.min.js', array( 'jquery' ), '20150519', true );
 
 }
